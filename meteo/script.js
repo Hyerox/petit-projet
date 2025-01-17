99789d10912e492397d104e8ca81c40b8b3f3c32
Hyerox <Hyerox@hotmail.com>
2025-01-17 08:36:04 +0100

    test

--- meteo/script.js
+++ meteo/script.js
@@ -1,10 +1,15 @@
 const apiKey = "f4e83224516fc36e24245ebe12c53981";
+<<<<<<< HEAD
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+=======
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+>>>>>>> dev
 const searchInput = document.querySelector(".search input");
 const searchBtn = document.getElementById("searchBtn");
 const weatherIcon = document.querySelector(".weather-icon");
 const condition = document.querySelector(".condition")
 
+<<<<<<< HEAD
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
@@ -50,6 +55,61 @@ searchInput.addEventListener("keydown", function(e) {
     }
 })
 
+=======
+async function checkWeather(apiUrl) {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+        const data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "images/cloudy.png";
+        } else if (data.weather[0].main === "Clear") {
+            weatherIcon.src = "images/clear-day.png";
+        } else if (data.weather[0].main === "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "images/light-rain.png";
+        } else if (data.weather[0].main === "Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if (data.weather[0].main === "Thunderstorm") {
+            weatherIcon.src = "images/thunderstorm.png";
+        } else if (data.weather[0].main === "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        condition.innerHTML = data.weather[0].main;
+
+    } catch (error) {
+        console.log(error);
+        searchInput.placeholder = "Enter an existing city";
+        searchInput.classList.add("placeholder-red");
+    }
+    searchInput.value = "";
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(validation(searchInput.value));    
+}) 
+
+
+
+searchInput.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        checkWeather(validation(searchInput.value))
+    }
+})
+
+
+>>>>>>> dev
 searchInput.addEventListener("focus", function() {
     searchInput.placeholder = "";
 })
@@ -58,7 +118,14 @@ searchInput.addEventListener("blur", function() {
     searchInput.placeholder = "Enter City Name"
 })
 
+<<<<<<< HEAD
 
+=======
+document.addEventListener("click", (e) => {
+    searchInput.classList.remove("placeholder-red")
+    searchInput.placeholder = "Enter City Name"
+})
+>>>>>>> dev
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -67,6 +134,13 @@ if (navigator.geolocation) {
             const longitude = position.coords.longitude;
 
             console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+<<<<<<< HEAD
+=======
+            
+            let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+            
+            checkWeather(apiUrl);
+>>>>>>> dev
         },
         (error) => {
             console.error(`Erreur lors de la géolocalisation : ${error.message}`);
@@ -74,4 +148,19 @@ if (navigator.geolocation) {
     );
 } else {
     console.log("La géolocalisation n'est pas supportée par ce navigateur.");
+<<<<<<< HEAD
+=======
+}
+
+function validation(userSearchTerm) {
+    userSearchTerm = userSearchTerm.trim();
+    
+    if (!isNaN(userSearchTerm)) {
+        searchInput.placeholder = "Enter an existing city"; 
+        searchInput.classList.add("placeholder-red");
+        return ""
+        
+    }
+    return userSearchTerm;
+>>>>>>> dev
 }
